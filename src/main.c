095de82eeb11adc5e9beb41cfdd5c3c89d58b606
TheoDacosta <theo.dacosta@ynov.com>
2024-03-14 15:19:16 +0100

    plusieurs taches dans main

--- src/main.c
+++ src/main.c
@@ -6,15 +6,35 @@
 #include "cmsis_os.h"
 
 osThreadId_t defaultTaskHandle;
+osThreadId_t Task1ID;
 
 void StartDefaultTask(void *argument);
 
+void StartDefaultTask(void *argument)
+{
+    while (1)
+    {
+        puts("Hello world\n");
+        osDelay(1000); // tache toute les secondes
+    }
+}
+
+void Tache1(void *argument)
+{
+    while (1)
+    {
+        puts("Tache 1 en cours");
+        osDelay(1500); // tache toute les 1,5 secondes
+    }
+}
+
 int main(void)
 {
+    // les initialisations
     hardware_init();
-
     osKernelInitialize();
 
+    // tache dafault
     const osThreadAttr_t defaultTask_attributes = {
         .name = "defaultTask",
         .priority = (osPriority_t)osPriorityNormal,
@@ -22,18 +42,28 @@ int main(void)
     };
     defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
+    if (defaultTaskHandle == NULL)
+    {
+        puts("Erreur lors de la création de la tache par défaut \n");
+    }
+
+    // tache 1
+    const osThreadAttr_t Task1_attributes = {
+        .name = "Task1",
+        .priority = (osPriority_t)osPriorityNormal1,
+        .stack_size = 128,
+    };
+    Task1ID = osThreadNew(Tache1, NULL, &Task1_attributes);
+
+    if (Task1ID == NULL)
+    {
+        puts("Erreur lors de la création de la tache 1 \n");
+    }
+
+    // démarrage du noyau
     osKernelStart();
 
     while (1)
     {
-    }
-}
-
-void StartDefaultTask(void *argument)
-{
-    while (1)
-    {
-        puts("Hello world\n");
-        osDelay(1);
     }
 }
