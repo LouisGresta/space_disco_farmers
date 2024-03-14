614ac59caeb7a640899ac8f47fe8f5ee4faf3ba0
Louis Gresta <louis.gresta@gmail.com>
2024-03-14 15:30:27 +0100

    move files

--- src/commands.c
+++ src/commands.c
@@ -5,16 +5,16 @@
 static char buffer[MAX_COMMAND_SIZE];
 
 const char* move_command(int ship_id, int angle, int speed) {
-    sprintf(buffer, MAX_COMMAND_SIZE, "MOVE %d %d %d\n", ship_id, angle, speed);
+    sprintf(buffer, "MOVE %d %d %d\n", ship_id, angle, speed);
     return buffer;
 }
 
 const char* fire_command(int ship_id, int angle) {
-    sprintf(buffer, MAX_COMMAND_SIZE, "FIRE %d %d\n", ship_id, angle);
+    sprintf(buffer, "FIRE %d %d\n", ship_id, angle);
     return buffer;
 }
 
 const char* radar_command(int ship_id) {
-    sprintf(buffer, MAX_COMMAND_SIZE, "RADAR %d\n", ship_id);
+    sprintf(buffer, "RADAR %d\n", ship_id);
     return buffer;
 }
