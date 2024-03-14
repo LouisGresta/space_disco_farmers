271c9d243c1f1088abd3c6471391bce45279c5dd
Louis Gresta <louis.gresta@gmail.com>
2024-03-14 23:36:49 +0100

    tests commands + debut parse radar response

--- src/commands.c
+++ src/commands.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_COMMAND_SIZE 100
 
 static char buffer[MAX_COMMAND_SIZE];
 
-const char* move_command(int ship_id, int angle, int speed) {
-    sprintf(buffer, "MOVE %d %d %d\n", ship_id, angle, speed);
+// Send commands to the server
+const char* move_command(uint32_t ship_id, uint16_t angle, uint16_t speed) {
+    sprintf(buffer, "MOVE %d %d %d", ship_id, angle, speed);
     return buffer;
 }
 
-const char* fire_command(int ship_id, int angle) {
-    sprintf(buffer, "FIRE %d %d\n", ship_id, angle);
+const char* fire_command(uint8_t ship_id, uint16_t angle) {
+    sprintf(buffer, "FIRE %d %d", ship_id, angle);
     return buffer;
 }
 
-const char* radar_command(int ship_id) {
-    sprintf(buffer, "RADAR %d\n", ship_id);
+const char* radar_command(uint8_t ship_id) {
+    sprintf(buffer, "RADAR %d", ship_id);
     return buffer;
+}
+
+// Split a string uinto an array of strings using strtok
+char** split(const char* str, const char delimiter) {
+    char delimiter_str[2];
+    delimiter_str[0] = delimiter;
+    delimiter_str[1] = '\0';
+    uint16_t count;
+    uint16_t i = 0;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == delimiter) {
+            count++;
+        }
+    }
+    count++;
+
+    char* token;
+    char** tokens = malloc(sizeof(char*) * count);
+    if (!tokens) return NULL;
+    i = 0;
+    char* tmp = strdup(str);
+    token = strtok(tmp, delimiter_str);
+    while (token != NULL) {
+        tokens[i] = token;
+        token = strtok(NULL, delimiter_str);
+        i++;
+    }
+    return tokens;
+}
+// Parse responses from the server
+int parse_radar_response(const char* response) {
+    char **str_scan = split(response, ',');
+    // don't forget to free the memory of str_scan
+    return 0;
 }
---
+++ test/test_commands.c
@@ -0,0 +1,44 @@
+#include "minunit.h"
+#include "../src/commands.c"
+
+MU_TEST(test_move_command) {
+    mu_assert_string_eq("MOVE 1 90 1000", move_command(1, 90, 1000));
+    mu_assert_string_eq("MOVE 6 180 2000", move_command(6, 180, 2000));
+    mu_assert_string_eq("MOVE 8 270 3000", move_command(8, 270, 3000));
+}
+
+MU_TEST(test_fire_command) {
+    mu_assert_string_eq("FIRE 1 90", fire_command(1, 90));
+    mu_assert_string_eq("FIRE 3 180", fire_command(3, 180));
+    mu_assert_string_eq("FIRE 5 270", fire_command(5, 270));
+}
+
+MU_TEST(test_radar_command) {
+    mu_assert_string_eq("RADAR 6", radar_command(6));
+    mu_assert_string_eq("RADAR 7", radar_command(7));
+}
+
+MU_TEST(test_split) {
+    char** tokens = split("Theo,Louis,Pachelle Carelle", ',');
+    // get lenght of tokens
+    uint8_t i = 0;
+    while (tokens[i] != NULL) i++;
+    mu_assert_int_eq(3, i);
+    mu_assert_string_eq("Theo", tokens[0]);
+    mu_assert_string_eq("Louis", tokens[1]);
+    mu_assert_string_eq("Pachelle Carelle", tokens[2]);
+    free(tokens);
+}
+
+MU_TEST_SUITE(test_suite) {
+    MU_RUN_TEST(test_move_command);
+    MU_RUN_TEST(test_fire_command);
+    MU_RUN_TEST(test_radar_command);
+    MU_RUN_TEST(test_split);
+}
+
+int main(void) {
+    MU_RUN_SUITE(test_suite);
+    MU_REPORT();
+    return 0;
+}
