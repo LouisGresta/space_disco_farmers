6bf6e96085051387db5755a264b942ff90ed6783
Louis Gresta <louis.gresta@gmail.com>
2024-04-12 10:01:48 +0200

    fix spaceships_mutex_attr

--- src/embedded/embedded_spaceship.c
+++ src/embedded/embedded_spaceship.c
@@ -1,14 +1,14 @@
 #include "embedded_spaceship.h"
 #include "os_utils.h"
 
-const osMutexAttr_t spaceships_mutex_attr = {"spaceshipsMutex",
-                                             osMutexPrioInherit, NULL, 0U};
+const osMutexAttr_t ship_mutex_attr = {"shipMutex", osMutexPrioInherit, NULL,
+                                       0U};
 
 void init_embedded_spaceship(Embedded_spaceship *embedded_spaceship,
                              Spaceship *spaceship, uint8_t index) {
   embedded_spaceship->spaceship = spaceship;
   embedded_spaceship->index = index;
-  embedded_spaceship->mutex_id = create_mutex(&spaceships_mutex_attr);
+  embedded_spaceship->mutex_id = create_mutex(&ship_mutex_attr);
 }
 
 void init_embedded_spaceships(Embedded_spaceship *embedded_spaceships,
